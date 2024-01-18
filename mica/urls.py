c8bce9c708285f3fb9abd0f3ab948832ea5e2665
aladin99 <aladin.dunp@gmail.com>
2024-01-18 21:00:38 +0100

    Add

--- mica/urls.py
+++ mica/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
+from .views import index
 
 urlpatterns = [
-    path('game/', include('game.urls'))
+    path('game/', include('game.urls')),
+    path('', index),
 ]
