90258a947822c82c2ca85faf6512c3c0e27c45a6
aladin99 <aladin.dunp@gmail.com>
2024-01-21 15:09:56 +0100

    Fix v1.2

--- mica/urls.py
+++ mica/urls.py
@@ -1,9 +1,5 @@
 from django.urls import path, include
-from  mica.views import index
-# from django.conf.urls.static import static
-# from django.conf import settings
 
 urlpatterns = [
-    path('game/', include('game.urls')),
-    path("index",index)
+    path('game/', include('game.urls'))
 ]
