25e00b7c49cdbcb402ada689f1fe4198df7dbeb8
aladin99 <aladin.dunp@gmail.com>
2024-01-21 13:54:41 +0100

    asdasdas

--- mica/urls.py
+++ mica/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
 from  mica.views import index
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('game/', include('game.urls')),
     path("index",index)
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
